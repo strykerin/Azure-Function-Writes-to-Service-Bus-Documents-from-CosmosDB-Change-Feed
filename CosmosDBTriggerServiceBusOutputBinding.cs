f101ef2a5d7128b0dea1171a5d4d36ab3b1546e7
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-10-28 16:52:32 -0300

    Write documentId into Service Bus

--- CosmosDBTriggerServiceBusOutputBinding.cs
+++ CosmosDBTriggerServiceBusOutputBinding.cs
@@ -9,7 +9,8 @@ namespace AzureFunctions
     public class ServiceBusOutputBinding
     {
         [FunctionName("ServiceBusQueueTriggerCSharp")]
-        public static void Run([CosmosDBTrigger(
+        [return: ServiceBus("queue1", Connection = "ServiceBusConnection")]
+        public static string Run([CosmosDBTrigger(
             databaseName: "devdatabase",
             collectionName: "bankotcexchange-transfero-collection",
             ConnectionStringSetting= "CosmosDBConnection",
@@ -17,6 +18,15 @@ namespace AzureFunctions
             LeaseCollectionPrefix = "ServiceBusOutput-")]IReadOnlyList<Document> documents,
             ILogger log)
         {
+            if (documents != null && documents.Count > 0)
+            {
+                foreach (Document document in documents)
+                {
+                    log.LogInformation($"DocumentId: {document.Id}");
+                }
+            }
+
+            return documents[0].Id;
         }
     }
 }
