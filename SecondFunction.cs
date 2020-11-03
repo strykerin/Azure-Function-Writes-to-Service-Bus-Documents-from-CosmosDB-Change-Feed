91d89ac81429b8a966eb68185629f61e6e1cef67
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-03 19:17:09 -0300

    Publish message to Topic with Label Email when condition is satisfied

--- SecondFunction.cs
+++ SecondFunction.cs
@@ -1,13 +1,26 @@
 using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using ServiceBusOutputFunction;
 
 namespace AzureFunctions
 {
     public class SecondFunction
     {
+        private readonly ITopicClient _topicClient;
+
+        public SecondFunction(ITopicClient topicClient)
+        {
+            _topicClient = topicClient;
+        }
+
         [FunctionName("SecondFunction")]
-        public static void Run([ServiceBusTrigger("queue1", Connection = "ServiceBusConnection")] 
+        public async Task Run(
+            [ServiceBusTrigger(queueName: "queue1", Connection = "ServiceBusConnection")] 
                                     string myQueueItem,
                                     Int32 deliveryCount,
                                     DateTime enqueuedTimeUtc,
@@ -18,6 +31,20 @@ namespace AzureFunctions
             log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
             log.LogInformation($"DeliveryCount={deliveryCount}");
             log.LogInformation($"MessageId={messageId}");
+
+            // Send string to Topic
+            Car car = JsonSerializer.Deserialize<Car>(myQueueItem);
+            if (car?.Wheels == 4)
+            {
+                await SendMessageEmailLabelAsync(myQueueItem);
+            }
+        }
+
+        private async Task SendMessageEmailLabelAsync(string messageBody)
+        {
+            Message message = new Message(Encoding.UTF8.GetBytes(messageBody));
+            message.Label = "Email";
+            await _topicClient.SendAsync(message);
         }
     }
 }
