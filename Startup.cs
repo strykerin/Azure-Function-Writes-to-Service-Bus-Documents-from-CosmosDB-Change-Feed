4febf073c50350e3d73e1f2d84fed149ee9f9cdc
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-03 17:25:33 -0300

    Create startup class

---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(ServiceBusOutputFunction.Startup))]
+namespace ServiceBusOutputFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Inject IConfiguration
+            // https://burhaantargett.tech/2019-07-16-azure-functions-di-and-configuration/
+            var localRoot = Environment.GetEnvironmentVariable("AzureWebJobsScriptRoot");
+            var azureRoot = $"{Environment.GetEnvironmentVariable("HOME")}/site/wwwroot";
+            var actualRoot = localRoot ?? azureRoot;
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(actualRoot)
+                .AddEnvironmentVariables()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = configBuilder.Build();
+            builder.Services.AddSingleton(configuration);
+        }
+    }
+}
