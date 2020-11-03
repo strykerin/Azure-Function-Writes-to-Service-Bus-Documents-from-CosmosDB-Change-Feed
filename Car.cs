555819a04b3b0e17091c9f3fc7fa5b1096415599
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-03 19:15:37 -0300

    Add id property in Car.cs

--- Car.cs
+++ Car.cs
@@ -1,7 +1,11 @@
-﻿namespace ServiceBusOutputFunction
+﻿using System.Text.Json.Serialization;
+
+namespace ServiceBusOutputFunction
 {
     public class Car
     {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
         public string Brand { get; set; }
         public int Wheels { get; set; }
     }
